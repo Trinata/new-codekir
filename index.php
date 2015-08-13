a54fd794e49fccc1e62c16982a4d73ee87d1b57e
Ovan Pulu <ovan89@gmail.com>
2015-08-13 15:17:42 +0700

    nambahin komentar di index
    nambahin komentar di index

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ session_start();
 
 define ('CODEKIR', 'Version 1.0');
 
+/* setting path application */
 define ('CODEKIR_ROOTPATH', '/home/ovancop/Data/htdocs/codekir/v1.1/');
 define ('CODEKIR_APPPATH', CODEKIR_ROOTPATH . 'applications/default/');
 
+/* setting path engine */
 define ('CODEKIR_COREPATH', '/home/ovancop/Data/htdocs/codekir/v1.1/loader/engine/');
 define ('CODEKIR_LIBS', '/home/ovancop/Data/htdocs/codekir/v1.1/loader/plugin/');
 define ('CODEKIR_LOGS', '/home/ovancop/Data/htdocs/codekir/v1.1/loader/logs/');
